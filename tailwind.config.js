9085c711ccf9777bac8282fef644226ba35cd960
wahyudwiprstka <wahyudwiprstka@gmail.com>
2024-08-27 13:11:35 +0800

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ export default {
         "./resources/**/*.vue",
         "./node_modules/flowbite/**/*.js",
         "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./public/**/*.blade.php",
+        "./public/**/*.php",
+        "./public/**/*.js",
+        "./public/**/*.vue",
     ],
     theme: {
         extend: {},
